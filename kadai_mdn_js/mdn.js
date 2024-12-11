5784de81e4f95d9c5047117ab24fe42e76e79f12
neco7373 <muuuuu73@gmail.com>
2024-12-11 14:23:34 +0900

    課題再再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,10 +1,9 @@
 
-const today = new Date("2024-12-11");
+const today = new Date();
 const [month, day, year] = [
   today.getMonth() +1,
   today.getDate(),
   today.getFullYear(),
 ];
-// [11, 12, 2024] （月は 0 基点であるため）
 
 console.log(year + '年' + month + '月' + day + '日');
